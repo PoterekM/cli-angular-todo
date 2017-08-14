4908f7b29531440ec9ca4a62cc18763d9d2815ac
Larry Taylor, Michelle Poterek and jacob ruleaux <larry+michelle+jacob@>
2017-08-14 11:44:02 -0700

    add first hardcoded task and use template to display it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Homework!';
+  currentFocus: string = "angular homework";
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  firstTask: Task = new Task("Finish weekend angular homework");
+}
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string) {}
 }
